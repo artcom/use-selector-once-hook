0f5a78659be78122c559c8b3e1b63facfc5d70f9
Philipp Niederlag <philipp.niederlag@artcom.de>
2021-07-05 20:30:09 +0200

    Use useRef hook to store the redux state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import { useCallback } from "react"
+import { useRef } from "react"
 import { useStore } from "react-redux"
 
 export function useSelectorOnce(selector) {
   const store = useStore()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getMemoizedState = useCallback(() => store.getState(), [])
-  const storeState = getMemoizedState()
-
-  return selector(storeState)
+  const state = useRef()
+  state.current = state.current || store.getState()
+  return selector(state.current)
 }
+
